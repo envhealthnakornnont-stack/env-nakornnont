7c86f26f458e9b29473b666ac8c635163b2792db
Chalermchai Wauwai <59055693+phetchalermchai@users.noreply.github.com>
2025-11-08 02:33:27 +0700

    Delete components directory

--- components/Divider.tsx
+++
@@ -1,8 +0,0 @@
-const Divider = () => (
-    <div className="flex w-full flex-col border-opacity-50">
-      <div className="divider"></div>
-    </div>
-  );
-  
-  export default Divider;
-  
--- components/News/Content/utils.ts
+++
@@ -1,9 +0,0 @@
-import { ApiTag } from "@/components/News/Content/api-types";
-
-export const mapTagsToSlugs = (tags?: ApiTag[] | null): string[] =>
-    (tags ?? [])
-        .map((t) => t.slug ?? t.name ?? "")
-        .filter((s): s is string => Boolean(s));
-
-export const resolveUpload = (src?: string | null) =>
-    !src ? null : src.startsWith("/uploads") ? `/api/uploads${src}` : src;
